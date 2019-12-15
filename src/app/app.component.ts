d8fc8b4b3238d1a991175dc3257fef24e38d2aa2
Ulrich Lehner <me@ulrichlehner.com>
2019-12-15 15:11:43 +0100

    improvement: better rendering

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { ViewerConfiguration } from 'angular-cesium';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [ViewerConfiguration]
 })
 export class AppComponent {
   title = 'alpenblick';
+
+  constructor(private viewerConf: ViewerConfiguration) {
+    this.viewerConf.viewerOptions = {
+      animation: false,
+      terrainShadows: Cesium.ShadowMode.ENABLED,
+      terrainProvider: Cesium.createWorldTerrain(),
+      timeline: false,
+      shadows: false
+    };
+
+    this.viewerConf.viewerModifier = viewer => {
+      viewer.scene.highDynamicRange = true;
+      viewer.scene.postProcessStages.fxaa.enabled = true;
+    };
+  }
 }

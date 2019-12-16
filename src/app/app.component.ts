c4d66998a8b63a724d258c2a4502ff5a4edef8e4
Ulrich Lehner <me@ulrichlehner.com>
2019-12-16 07:50:55 +0100

    improvement: refined rendering and display Ion alps viewshed layer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ export class AppComponent {
   constructor(private viewerConf: ViewerConfiguration) {
     this.viewerConf.viewerOptions = {
       animation: false,
-      terrainShadows: Cesium.ShadowMode.ENABLED,
+      // terrainShadows: Cesium.ShadowMode.ENABLED,
       terrainProvider: Cesium.createWorldTerrain(),
       timeline: false,
       shadows: false
@@ -20,6 +20,27 @@ export class AppComponent {
     this.viewerConf.viewerModifier = viewer => {
       viewer.scene.highDynamicRange = true;
       viewer.scene.postProcessStages.fxaa.enabled = true;
+      viewer.scene.fog.density = 8.0e-5;
+      viewer.scene.skyAtmosphere.hueShift = -0.08;
+      viewer.scene.skyAtmosphere.saturationShift = -0.3;
+      viewer.scene.skyAtmosphere.brightnessShift = -0.2;
+
+      const imageryLayers = viewer.imageryLayers;
+      if (imageryLayers.length > 0) {
+        const layer = imageryLayers.get(0);
+        layer.brightness = 0.4;
+        layer.contrast = 1.2;
+        layer.hue = 0;
+        layer.saturation = 0.3;
+        layer.gamma = 0.8;
+      }
+
+      // Austrian Alps Viewshed 100 km
+      const imageryLayer = viewer.imageryLayers.addImageryProvider(
+        new Cesium.IonImageryProvider({ assetId: 61602 })
+      );
+
+      viewer.zoomTo(imageryLayer);
     };
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,10 @@ if (environment.production) {
 }
 
 Cesium.buildModuleUrl.setBaseUrl('/assets/cesium/');
-//Cesium.Ion.defaultAccessToken="";
+Cesium.Ion.defaultAccessToken =
+  // tslint:disable-next-line: max-line-length
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJhMzMzZTg1ZS04YjgyLTQ4NjYtYjc3Zi1iMmExNDdmMjllMjEiLCJpZCI6MTk4ODcsInNjb3BlcyI6WyJhc3IiLCJnYyJdLCJpYXQiOjE1NzY0MjE4NTd9.uaXQ6d6AORmZ2mG-KvJpZfBU1BWNX4JEAo9Kp-jfB0Y';
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
   .catch(err => console.error(err));

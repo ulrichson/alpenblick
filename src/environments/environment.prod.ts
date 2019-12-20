bfe08e98da678de2b071538bebef59d9a113b03a
Ulrich Lehner <me@ulrichlehner.com>
2019-12-20 18:25:31 +0100

    fix: correct analytics url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ import { Environment } from 'src/app/shared/model/environment';
 export const environment: Environment = {
   production: true,
   matomo: {
-    url: 'https://analytics.inspirespace.co',
+    url: 'https://analytics.inspirespace.co/',
     id: 3
   }
 };
